dfca676367fe526fa57508e0c95ea2a70c9cd4d2
Sam Bray <sam.bray@ucsf.edu>
2024-11-08 11:37:49 -0800

    add encoder model with independence between shanks

--- src/c3po/model/encoder.py
+++ src/c3po/model/encoder.py
@@ -8,6 +8,7 @@ from functools import partial
 
 from .util import MLP
 
+
 class BaseEncoder(nn.Module):
     latent_dim: int
 
@@ -17,13 +18,18 @@ class BaseEncoder(nn.Module):
     def __call__(self, x):
         raise NotImplementedError
 
+
 def encoder_factory(encoder_model: str, latent_dim: int, **kwargs) -> BaseEncoder:
     if encoder_model == "simple":
         return SimpleEncoder(latent_dim=latent_dim, **kwargs)
     if encoder_model == "convolutional1D":
         return convolutionalEncoder1D(latent_dim=latent_dim, **kwargs)
+    if encoder_model == "multi_shank":
+        return MultiShankEncoder(latent_dim=latent_dim, **kwargs)
+
     else:
         raise ValueError(f"Unknown encoder model: {encoder_model}")
+
 
 class SimpleEncoder(BaseEncoder):
     widths: Sequence[int]
@@ -36,19 +42,27 @@ class SimpleEncoder(BaseEncoder):
     def __call__(self, x):
         return self.encoder(x)
 
+
 class convolutionalEncoder1D(BaseEncoder):
     conv_kernel_sizes: Sequence[int]
     conv_strides: Sequence[int]
     conv_features: Sequence[int]
     widths: Sequence[int]
 
-
     def setup(self):
-        if not len(self.conv_kernel_sizes) == len(self.conv_strides) == len(self.conv_features):
-            raise ValueError("conv_kernel_sizes and conv_strides must have the same length")
+        if (
+            not len(self.conv_kernel_sizes)
+            == len(self.conv_strides)
+            == len(self.conv_features)
+        ):
+            raise ValueError(
+                "conv_kernel_sizes and conv_strides must have the same length"
+            )
         conv_layers = []
         print("HI")
-        for i,(kernel_size, stride, features) in enumerate(zip(self.conv_kernel_sizes, self.conv_strides, self.conv_features)):
+        for i, (kernel_size, stride, features) in enumerate(
+            zip(self.conv_kernel_sizes, self.conv_strides, self.conv_features)
+        ):
             # if i == 0:
             #     kernel = kernel_size
             # else:
@@ -70,5 +84,37 @@ class convolutionalEncoder1D(BaseEncoder):
             x = layer(x)
 
         print(x.shape)
-        x = jax.lax.reshape(x, (x.shape[0], x.shape[1]*x.shape[2]))
+        x = jax.lax.reshape(x, (x.shape[0], x.shape[1] * x.shape[2]))
         return self.dense(x)
+
+
+class MultiShankEncoder(BaseEncoder):
+    """makes a seperate encoder model for each shank then combines"""
+
+    n_shanks: int
+    shank_encoder_params: dict
+    latent_dim: int
+
+    def setup(self):
+        self.shank_encoders = [
+            encoder_factory(**self.shank_encoder_params, latent_dim=self.latent_dim)
+            for _ in range(self.n_shanks)
+        ]
+
+    def __call__(self, x):
+        print("x shape", x.shape)
+        print("x shape", x[..., 0].shape)
+        encoded_shanks = [
+            encoder(x[..., i]) for i, encoder in enumerate(self.shank_encoders)
+        ]
+        print("encoded shanks shape", encoded_shanks[0].shape)
+        encoded_shanks = jnp.stack(encoded_shanks, axis=-1)
+        print("encoded shanks shape", encoded_shanks.shape)
+        shank_indicator = jnp.sum(jnp.abs(x), axis=-2)
+        print("shank indicator shape", shank_indicator.shape)
+        shank_indicator = jnp.where(shank_indicator != 0, 1.0, 0.0)
+        print("shank indicator shape", shank_indicator.shape)
+        encoded_shanks = jnp.sum(encoded_shanks * shank_indicator[:, None, :], axis=-1)
+        # encoded_shanks = jnp.sum(encoded_shanks, axis=-1)
+        print("encoded shanks shape", encoded_shanks.shape)
+        return encoded_shanks
