a973f8f99c460859fbeccf8a606f0cd4b4c2cc02
samuelbray32 <sam.bray@ucsf.edu>
2025-09-03 14:38:44 -0700

    tools for easy io of model and results within the analysis object

---
+++ src/c3po/analysis/linear_assemblies.py
@@ -0,0 +1,194 @@
+# import datajoint as dj
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import pandas as pd
+# import pynwb
+import scipy
+from sklearn.decomposition import PCA, FastICA
+
+from spyglass.common import AnalysisNwbfile, IntervalList
+from spyglass.spikesorting.analysis.v1.group import SortedSpikesGroup
+from spyglass.utils.dj_mixin import SpyglassMixin
+
+# schema = dj.schema("spikesorting_linear_assembly_v1")
+
+
+def squared_assembly_activity(ic_vectors, Z):
+    """
+    Compute assembly activity from IC vectors and spike vector
+
+    Parameters
+    ----------
+    ic_vectors : np.ndarray
+        IC vectors (num_assemblies x num_neurons)
+    Z : np.ndarray
+        Spike vector (num_bins x num_neurons)
+
+    Returns
+    -------
+    np.ndarray
+        Assembly activity (num_bins x num_assemblies)
+    """
+    return np.square(ic_vectors @ Z.T).T
+
+
+def diagonal_corrected_assembly_activity(ic_vectors, Z):
+    """
+    Compute assembly activity from IC vectors and spike vector
+
+    Parameters
+    ----------
+    ic_vectors : np.ndarray
+        IC vectors (num_assemblies x num_neurons)
+    Z : np.ndarray
+        Spike vector (num_bins x num_neurons)
+
+    Returns
+    -------
+    np.ndarray
+        Assembly activity (num_bins x num_assemblies)
+    """
+    assembly_activities = np.zeros(
+        (Z.shape[0], ic_vectors.shape[0])
+    )  # [n_time_bins x n_assemblies]
+    for i, vec in enumerate(ic_vectors):  # vec: [n_units]
+        # Normalize vec if not already
+        vec = vec / np.linalg.norm(vec)
+        # Signed linear projection: [n_time_bins]
+        signed_proj = Z @ vec
+        # Outer product with diagonal removed
+        W = np.outer(vec, vec)
+        np.fill_diagonal(W, 0)
+        # Quadratic form: [n_time_bins]
+        quadratic_form = np.einsum("ti,ij,tj->t", Z, W, Z)
+        # Signed magnitude: sign * sqrt(abs)
+        signed_magnitude = np.sign(signed_proj) * np.sqrt(np.abs(quadratic_form))
+        assembly_activities[:, i] = signed_magnitude
+    return assembly_activities
+
+
+activation_metric_dictionary = {
+    "squared": squared_assembly_activity,
+    "diagonal_corrected": diagonal_corrected_assembly_activity,
+}
+
+
+def compute_linear_assembly(spike_vector, activation_metric="squared"):
+    """
+    Reference:
+        Detecting cell assemblies in large neuronal populations
+        Vítor Lopes-dos-Santos, Sidarta Ribeiro, Adriano B L Tort
+        J Neurosci Methods 2013
+        https://doi.org/10.1016/j.jneumeth.2013.04.010
+    """
+    Z = scipy.stats.zscore(spike_vector, axis=0)
+    Z[np.isnan(Z)] = 0
+    # compute eigenvalue bounds of Marcenko-Pastur
+    n_rows, n_cols = Z.shape
+    q = n_rows / n_cols
+    # lambda_min = (1 - np.sqrt(1 / q)) ** 2
+    lambda_max = (1 + np.sqrt(1 / q)) ** 2
+    # compute PCA and number of significant assemblies
+    pca = PCA()
+    Z_proj_pca = pca.fit_transform(Z)  # (num_bins) x (num_pcs)
+    pc_vectors = pca.components_
+    eigenvalues = pca.explained_variance_
+    signif_pc_indices = np.where(eigenvalues > lambda_max)[0]
+    # print(f"Epoch {epoch}, num assemblies: {len(signif_pc_indices)}")
+    Z_signif_proj_pca = Z_proj_pca[:, signif_pc_indices]  # retain only significant PCs
+    # compute ICA in PC-reduced space
+    ica = FastICA(n_components=len(signif_pc_indices), random_state=0)
+    ica.fit(Z_signif_proj_pca)
+    V = pca.components_[signif_pc_indices, :]
+    ic_vectors = np.dot(ica.components_, V)  # project ICs back to original unit space
+    # normalize weights by norm
+    for k in range(len(ic_vectors)):
+        ic_vectors[k] = ic_vectors[k] / np.linalg.norm(ic_vectors[k])
+        pc_vectors[k] = pc_vectors[k] / np.linalg.norm(pc_vectors[k])
+    # By convention, want largest magnitude weight to be positive -- flip sign if necessary
+    for k in range(len(ic_vectors)):
+        ic_vectors[k] = ic_vectors[k] * np.sign(
+            ic_vectors[k][np.argmax(np.abs(ic_vectors[k]))]
+        )
+        pc_vectors[k] = pc_vectors[k] * np.sign(
+            pc_vectors[k][np.argmax(np.abs(pc_vectors[k]))]
+        )
+
+    # compute assembly activity
+    ic_assembly_activities = activation_metric_dictionary[activation_metric](
+        ic_vectors, Z
+    )
+    # sort IC vectors by decreasing variance
+    row_variances = np.var(ic_assembly_activities, axis=0)
+    row_order = np.argsort(row_variances)[::-1]
+    ic_vectors = ic_vectors[row_order]  # sort in descending order
+    ic_assembly_activities = ic_assembly_activities[:, row_order]
+
+    return pc_vectors, eigenvalues, ic_vectors, ic_assembly_activities
+    # # Save results to NWB file
+    # analysis_file_name = AnalysisNwbfile().create(key["nwb_file_name"])
+    # key["analysis_file_name"] = analysis_file_name
+    # key["ic_vectors_object_id"] = AnalysisNwbfile().add_nwb_object(
+    #     analysis_file_name,
+    #     ic_vectors,
+    #     "ic_vectors",
+    # )
+    # key["pc_vectors_object_id"] = AnalysisNwbfile().add_nwb_object(
+    #     analysis_file_name,
+    #     pc_vectors,
+    #     "pc_vectors",
+    # )
+    # key["eigenvalues_object_id"] = AnalysisNwbfile().add_nwb_object(
+    #     analysis_file_name,
+    #     eigenvalues,
+    #     "eigenvalues",
+    # )
+    # assembly_activity_obj = pynwb.TimeSeries(
+    #     name="assembly_activity",
+    #     data=ic_assembly_activities,
+    #     unit="a.u.",
+    #     timestamps=t_spikes,
+    #     description="Assembly activity",
+    # )
+    # key["assembly_activities_object_id"] = AnalysisNwbfile().add_nwb_object(
+    #     analysis_file_name,
+    #     assembly_activity_obj,
+    # )
+    # AnalysisNwbfile().add(key["nwb_file_name"], analysis_file_name)
+
+    # self.insert1(key)
+
+
+# def plot_assembly_vectors(self):
+#     """
+#     Plot assembly vectors
+
+#     Returns
+#     -------
+#     fig : matplotlib.figure.Figure
+#         Each assembly vector is plotted in a separate subplot.
+#     """
+#     if not len(self) == 1:
+#         raise ValueError("plot_assembly_vectors() only works for single keys")
+#     nwb = self.fetch_nwb()[0]
+#     ic = nwb["ic_vectors"].data[:].T
+#     x = np.arange(ic.shape[0])
+
+#     i = 0
+#     fig, ax = plt.subplots(
+#         nrows=ic.shape[1], ncols=1, sharex=True, figsize=(5, ic.shape[1])
+#     )
+#     for i, a in enumerate(ax):
+#         a.scatter(x, ic[:, i], c="k", s=30)
+#         a.vlines(
+#             x,
+#             np.clip(ic[:, i], ic[:, i], 0),
+#             np.clip(ic[:, i], 0, ic[:, i]),
+#             color="k",
+#             alpha=0.5,
+#         )
+#         a.spines[["top", "right", "bottom"]].set_visible(False)
+#         a.set_ylabel(f"Assem. {i}")
+#     ax[-1].set_xlabel("Neuron index")
+#     return fig
